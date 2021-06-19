2ca92f7adfde02a1c4d885c10b0f5a26cc399997
Saori Nakagami <someyasaori@gmail.com>
2021-06-20 06:54:07 +0900

    6/20 6:50

--- kadai/select.php
+++ kadai/select.php
@@ -19,11 +19,11 @@ try {
 $stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
 
 //検索バージョン（検索条件なければ一覧を表示）
-if($keyword!="" OR $search_tag!=""){
-    $stmt =$pdo->prepare ("SELECT * FROM gs_bm_table WHERE details LIKE '%$keyword%' OR WHERE tag=$search_tag");
-}else{
-    $stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
-}
+// if($keyword!="" OR $search_tag!=""){
+//     $stmt =$pdo->prepare ("SELECT * FROM gs_bm_table WHERE details LIKE '%$keyword%' OR WHERE tag=$search_tag");
+// }else{
+//     $stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
+// }
 
 // $pdo = new PDO($dsn, $username, $password, $options);
 //         if(@$_POST["id"] != "" OR @$_POST["user_name"] != ""){ //IDおよびユーザー名の入力有無を確認
@@ -35,25 +35,34 @@ if($keyword!="" OR $search_tag!=""){
 $status = $stmt->execute();
 
 //データを繰り返し処理で取り出し（表示はHTMLで）
+// $title_view = "";
+// $url_view = "";
+// $details_view = "";
+// $tag_view = "";
+// $indate_view = "";
+
+$view="";
 
 if($status==false){
     $error = $stmt->errorInfor();
     exit("ErrorQuery:". $error[2]);
 }else{
     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $title_view = $result['title'];
-        $url_view = $result['url'];
-        $details_view = $result['details'];
-        $tag_view = $result['tag'];
-        $indate_view = $result['indate']
-
+        // $title_view = $result['title'];
+        // $url_view = $result['url'];
+        // $details_view = $result['details'];
+        // $tag_view = $result['tag'];
+        // $indate_view = $result['indate']
+        $view .= "<p>";
+        $view .= $result['title'].':'.$result['url'].' '.$result['details'].' '.$result['tag'];
+        $view .= "</p>";
     }
 }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -63,13 +72,15 @@ if($status==false){
 <body>
 
 <p>検索結果表示</p> 
-<tr>
+<!-- <tr>
 	<td ><?=$title_view ?></td>
 	<td><?=$url_view ?></td>
 	<td><?=$details_view ?></td>
 	<td><?=$tag_view ?></td>
 	<td><?=$indate_view ?></td>
-</tr>
+</tr> -->
+
+<?= $view ?>
 
 </body>
 </html>
