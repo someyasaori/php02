e1c4216daa35fe59aff9b00986ae07a4d3d2f51e
Saori Nakagami <someyasaori@gmail.com>
2021-06-20 07:40:55 +0900

    6/20 7:40 JS version

--- kadai/select.php
+++ kadai/select.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お役立ち資料一覧</title>
+    <script src="js/jquery-2.1.3.min.js"></script>
+</head>
+<body>
+
+<h1>検索結果表示</h1>
+
+<table id ="summary" border='3'> 
+<tr>
+	<th>タイトル</th>
+	<th>URL</th>
+	<th>詳細</th>
+	<th>タグ</th>
+	<th>登録日時</th>
+</tr>
+<button id="search">検索</button>
+
+<!-- <?= $view ?> -->
+
+
+
 <?php
 
 //XSS対策
@@ -35,11 +62,11 @@ $stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
 $status = $stmt->execute();
 
 //データを繰り返し処理で取り出し（表示はHTMLで）
-// $title_view = "";
-// $url_view = "";
-// $details_view = "";
-// $tag_view = "";
-// $indate_view = "";
+$title_view = "";
+$url_view = "";
+$details_view = "";
+$tag_view = "";
+$indate_view = "";
 
 $view="";
 
@@ -48,20 +75,20 @@ if($status==false){
     exit("ErrorQuery:". $error[2]);
 }else{
     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // $title_view = $result['title'];
-        // $url_view = $result['url'];
-        // $details_view = $result['details'];
-        // $tag_view = $result['tag'];
-        // $indate_view = $result['indate']
-        $view .= "<p>";
-        $view .= $result['title'].':'.$result['url'].' '.$result['details'].' '.$result['tag'];
-        $view .= "</p>";
+        $title_view .= $result['title'];
+        $url_view .= $result['url'];
+        $details_view .= $result['details'];
+        $tag_view .= $result['tag'];
+        $indate_view .= $result['indate'];
+        // $view .= "<p>";
+        // $view .= $result['title'].':'.$result['url'].' '.$result['details'].' '.$result['tag'];
+        // $view .= "</p>";
     }
 }
 
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
@@ -71,16 +98,38 @@ if($status==false){
 </head>
 <body>
 
-<p>検索結果表示</p> 
+<p>検索結果表示</p>  -->
+
+<script>
+console.log(<?=$title_view ?>);
+
+$("#search").on("click",function(){
+let summary = '<tr><td>'+<?=$title_view ?>+'</td><td>'+<?=$url_view ?>+'</td><td>'+<?=$details_view ?>+'</td><td>'+<?=$tag_view ?>+'</td><td>'+<?=$indate_view ?>+'</td></tr>';
+console.log(summary);
+$("#view").append(summary);
+});
+
+</script>
+
 <!-- <tr>
-	<td ><?=$title_view ?></td>
+	<td><?=$title_view ?></td>
 	<td><?=$url_view ?></td>
 	<td><?=$details_view ?></td>
 	<td><?=$tag_view ?></td>
 	<td><?=$indate_view ?></td>
 </tr> -->
 
-<?= $view ?>
+<!-- <table border='6'> 
+<tr>
+	<th>タイトル</th>
+	<td>URL</td>
+	<td>詳細</td>
+	<td>タグ</td>
+	<td>登録日時</td>
+</tr> -->
 
+<!-- <?= $view ?> -->
+
+</table>
 </body>
 </html>
